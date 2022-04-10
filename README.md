aab9c118130430d878986bea04dde2ce88af22b6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-10 22:16:26 +0530

    Added Serilog

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ API Gateway using Ocelot for eshop solution
 
 ## Ocelot API Gateway to Products API
 
-![Execute With IIS Express |150x150](./Documentation/Images/OcelotApiGW_Products.PNG)
+![Ocelot Api Gateay to retrieve Products |150x150](./Documentation/Images/OcelotApiGW_Products.PNG)
+
+## Ocelot API Gateway to Products API
+
+![Serilog Log File 1 |150x150](./Documentation/Images/Serilog_LogFile1.PNG)
+
+
--- Source/ApiGateway.OcelotGw/Program.cs
+++ Source/ApiGateway.OcelotGw/Program.cs
@@ -1,6 +1,7 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Cache.CacheManager;
 using Ocelot.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +10,23 @@ builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
     config.AddJsonFile($"./Configuration/ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
 });
 
-builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
+//builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+//builder.Logging.AddConsole();
+//builder.Logging.AddDebug();
+
+string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm: ss.fff} [{Level}] { Message} { NewLine} { Exception}";
+
+// Add services to the container.
+var logger = new LoggerConfiguration()
+    .WriteTo.Debug()
+    .WriteTo.Console()
+    //.WriteTo.File("./Logs/Demo.txt", rollingInterval: RollingInterval.Day, outputTemplate: outputTemplate)
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 
