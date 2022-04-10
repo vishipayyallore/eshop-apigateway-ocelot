25aeeb68918ba83ce805a380eb836d2c7eb6d461
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-10 19:30:41 +0530

    Adding Routes

--- Source/ApiGateway-Ocelot/Program.cs
+++ Source/ApiGateway-Ocelot/Program.cs
@@ -1,6 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Cache.CacheManager;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
